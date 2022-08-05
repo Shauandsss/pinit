277dba805eeca22c166d986db77d7aca0dd09294
shauan <shauandsss>
2022-08-05 16:43:04 -0300

    gh-pages-2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as React from "react";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <Router>
+        <h1>testing</h1>
+        <hr />
+        <a href="https://api.instagram.com/oauth/authorize?client_id=968783993938106&redirect_uri=https://shauandsss.github.io/pinit/map&scope=user_profile,user_media,instagram_graph_user_profile&response_type=code">
+          Authorize on Instagram
         </a>
-      </header>
+
+        <Route exact path="/map">
+          <div>Access</div>
+          {console.log(new URLSearchParams(window.location.search).get("id"))}
+          {new URLSearchParams(window.location.search).get("id")}
+          {console.log(new URLSearchParams(window.location.search).get("code"))}
+          {new URLSearchParams(window.location.search).get("code")}
+        </Route>
+      </Router>
     </div>
   );
 }
