bb7d3d1225cf1cd085b869d3fc732b4c59ff1a50
shauan <shauandsss>
2022-08-05 23:44:13 -0300

    teste cors 2

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
-  const [shortLiveToken, setShortLiveToken] = useState(
-    "AQACrQNbAhRPF9BE26Whespmr7UGIa309zwNiowxpiVlJDRHF-1Wknuhb7fyS8lrTddw3YSKbWe8UjUGWV6XJq26hoObQRjgtOxRo8bsLZBNZSU0Lr_3sTXFlhT3raGEK7E7cqdMB0g8qohMAt-Bfl693h3aWIPvspfEPYjVhWUr5b2Q2oYLx_nTLJVg3lZNP7cn-czIrmJq8czOlvu_PAWPGbf_uWTErfz2l4hAnmDsbw"
-  );
+  const [shortLiveToken, setShortLiveToken] = useState("");
   const [longLiveToken, setLongLiveToken] = useState("");
 
+  //const fetchUserInfo = () => {};
   useEffect(() => {
-    //setShortLiveToken(new URLSearchParams(window.location.search).get("code"));
-
-    if (shortLiveToken !== "") {
-      console.log("Trocando Token");
+    if (!shortLiveToken)
+      setShortLiveToken(
+        new URLSearchParams(window.location.search).get("code")
+      );
+    console.log("Short:");
+    console.log(shortLiveToken);
+    console.log("Long:");
+    console.log(longLiveToken);
+    if (shortLiveToken && !longLiveToken) {
       var details = {
         client_id: 968783993938106,
         redirect_uri: "https://localhost:3000/",
@@ -36,16 +40,20 @@ function App() {
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
           "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "POST",
+          "access-control-request-headers": "access-control-allow-origin",
         },
         body: formBody,
       };
       fetch("https://api.instagram.com/oauth/access_token", requestOptions)
         .then((response) => response.json())
-        .then((data) => setLongLiveToken(data));
-      console.log(longLiveToken);
+        .then((data) => setLongLiveToken(data))
+        .then((data) => console.log(data));
     }
-  }, [longLiveToken, shortLiveToken]);
-  //const fetchUserInfo = () => {};
+  }, []);
+
+  console.log(shortLiveToken);
+  console.log(longLiveToken);
 
   return (
     <div className="App">
